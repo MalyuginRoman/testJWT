a3d1934b6083e73d990ad736b1acc2c11e720a08
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-05 16:55:11 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,5 @@
-#include <iostream>
-#include "jwt/jwt.hpp"
+#include <jwt-cpp/jwt.h>
 
 int main() {
-  using namespace jwt::params;
-
-  auto key = "secret"; //Secret to use for the algorithm
-  //Create JWT object
-  jwt::jwt_object obj{algorithm("HS256"), payload({{"some", "payload"}}), secret(key)};
-
-  //Get the encoded string/assertion
-  auto enc_str = obj.signature();
-  std::cout << enc_str << std::endl;
-
-  //Decode
-  auto dec_obj = jwt::decode(enc_str, algorithms({"HS256"}), secret(key));
-  std::cout << dec_obj.header() << std::endl;
-  std::cout << dec_obj.payload() << std::endl;
-
-  return 0;
+    jwt::decode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCJ9.AbIJTDMFc7yUa5MhvcP03nJPyCPzZtQcGEp-zWfOkEE");
 }
