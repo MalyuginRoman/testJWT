e5e3c1833b502a7b06947de7808fdacf95fe1489
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-17 16:41:48 +0500

    Create testJWT.cpp

---
+++ testJWT.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "math.h"
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <jwt-cpp/jwt.h>
+
+class TestJWT : public CPPUNIT_NS::TestCase
+{
+CPPUNIT_TEST_SUITE(TestJWT);
+  CPPUNIT_TEST(testJWT_1);
+CPPUNIT_TEST_SUITE_END();
+
+public:
+void setUp(void) {}
+void tearDown(void){}
+
+protected:
+  void testJWT_1(void)
+  {
+    std::cout << std::endl;
+    std::cout << jwt::decode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCJ9.AbIJTDMFc7yUa5MhvcP03nJPyCPzZtQcGEp-zWfOkEE") << std::endl;
+    std::cout << std::endl;
+  }
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TestJWT);
+
+int main()
+{
+CPPUNIT_NS::TestResult controller;
+
+CPPUNIT_NS::TestResultCollector result;
+controller.addListener(&result);
+
+CPPUNIT_NS::BriefTestProgressListener progress;
+controller.addListener(&progress);
+
+CPPUNIT_NS::TestRunner runner;
+runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+runner.run(controller);
+
+return result.wasSuccessful() ? 0 : 1;
+}
