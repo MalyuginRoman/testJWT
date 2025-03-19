4ab2d466b1e17a275aba65fb2dfc5ef4eb9a00b5
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-19 09:12:45 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <map>
+#include <chrono>
 #include <jwt/jwt.hpp>
 
 int main() {
-#ifdef testJWT
   using namespace jwt::params;
-  const char* enc_str =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MTM4NjIzNzEsImlkIjoiYS1iLWMtZC1lLWYtMS0yLTMiLCJpc3MiOiJhcnVuLm11cmFsaWRoYXJhbiIsInN1YiI6ImFkbWluIn0.jk7bRQKTLvs1RcuvMc2B_rt6WBYPoVPirYi_QRBPiuk";
+  jwt::jwt_object obj{algorithm("HS256"), secret("secret")};
+  
+  obj.add_claim("iss", "PetrPetrov")
+     .add_claim("sub", "admin")
+     .add_claim("id", "a-b-c-d-e-f-1-2-3")
+     .add_claim("iat", 1513862371)
+     .add_claim("exp", std::chrono::system_clock::now());
+  
   std::error_code ec;
   auto obj = jwt::decode(enc_str, algorithms({"none", "HS256"}), ec, verify(false), secret("secret"));
-#endif
 }
